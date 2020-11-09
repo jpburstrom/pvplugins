7eae19f90fa1266610ae5e9ca5dfd9e3309dac0c
Johannes Burström <johannes@ljud.org>
2020-11-09 21:26:39 +0100

    Add FilterPartials and SplitPartials

--- plugins/PVPlugins/PVPlugins.cpp
+++ plugins/PVPlugins/PVPlugins.cpp
@@ -1,33 +1,147 @@
 // PluginPVPlugins.cpp
 // Johannes Burström (johannes@ljud.org)
 
-#include "SC_PlugIn.hpp"
-#include "PVPlugins.hpp"
+#include "SC_PlugIn.h"
+#include "FFT_UGens.h"
 
-static InterfaceTable* ft;
+#define DefinePVUnit(name) \
+	(*ft->fDefineUnit)(#name, sizeof(PV_Unit), (UnitCtorFunc)&name##_Ctor, 0, 0);
 
-namespace PVPlugins {
+#define MaybeIncrementPartial(i) \
+    if (currentPartial < partials && i > binEnd) { \
+        ++currentPartial; \
+        CalculateBins; \
+    }
 
-PVPlugins::PVPlugins() {
-    mCalcFunc = make_calc_function<PVPlugins, &PVPlugins::next>();
-    next(1);
+#define CalculateBins \
+    binStart = (int)sc_ceil(freq * currentPartial * binWidthR) - (bandwidth >> 1) - 1; \
+    binEnd = binStart + bandwidth;
+    //Print("partial: %if, binStart: %i, binEnd: %i\n", currentPartial, binStart, binEnd);
+
+InterfaceTable* ft;
+
+struct PV_FilterPartials : PV_Unit {
+    SndBuf* m_buf;
+    float m_binWidth;
+};
+
+
+struct PV_SplitPartials : PV_Unit {
+    SndBuf* m_buf;
+    float m_binWidth;
+};
+
+
+static void PV_FilterPartials_next(PV_FilterPartials* unit, int inNumSamples);
+static void PV_FilterPartials_Ctor(PV_FilterPartials* unit);
+
+static void PV_SplitPartials_next(PV_SplitPartials* unit, int inNumSamples);
+static void PV_SplitPartials_Ctor(PV_SplitPartials* unit);
+
+void PV_FilterPartials_Ctor(PV_FilterPartials* unit) {
+	SETCALC(PV_FilterPartials_next);
+	ZOUT0(0) = ZIN0(0);
+
 }
 
-void PVPlugins::next(int nSamples) {
-    const float* input = in(0);
-    const float* gain = in(0);
-    float* outbuf = out(0);
+void PV_FilterPartials_next(PV_FilterPartials* unit, int inNumSamples) {
 
-    // simple gain function
-    for (int i = 0; i < nSamples; ++i) {
-        outbuf[i] = input[i] * gain[i];
+    float freq = IN0(1);
+    int partials = (int)IN0(2);
+    int mode = (int)IN0(4); // 0 = reject, 1 = select
+
+    PV_GET_BUF
+
+    float binWidthR = buf->samples / FULLRATE;
+    //Convert bandwidth factor of freq to bins
+    int bandwidth = (int)(freq * IN0(3) * binWidthR);
+    int binStart, binEnd, currentPartial=1;
+
+    Print("bandwidth: %i\n", bandwidth);
+
+    CalculateBins;
+
+    //SCPolarBuf *p = ToPolarApx(buf);
+	SCComplexBuf *p = (SCComplexBuf*)buf->data;
+
+    if (mode == 0) {
+        for (int i = 0; i < numbins; ++i) {
+
+            MaybeIncrementPartial(i);
+
+            if (i >= binStart && i < binEnd) {
+                //p->bin[i].mag =  p->bin[i].mag * 0.f;
+                p->bin[i] = 0.f;
+            }
+            //If we haven't succeeded in incrementing, no need to continue loop
+            if ( i > binEnd ) {
+                break;
+            }
+        }
+    } else {
+        for (int i = 0; i < numbins; ++i) {
+
+            MaybeIncrementPartial(i);
+
+            if (i < binStart) {
+                //p->bin[i].mag =  p->bin[i].mag * 0.f;
+                p->bin[i] = 0.f;
+            }
+            //If we haven't succeeded in incrementing, no need to continue loop
+            if ( i > binEnd ) {
+                break;
+            }
+        }
+
+        //Zero the rest of the partials
+        for (int i = binEnd; i < numbins; ++i) {
+            p->bin[i] = 0.f;
+        }
+
+        p->nyq = 0.f;
+        p->dc = 0.f;
+    }
+
+}
+
+void PV_SplitPartials_Ctor(PV_SplitPartials* unit) {
+	SETCALC(PV_SplitPartials_next);
+	ZOUT0(0) = ZIN0(0);
+    ZOUT0(1) = ZIN0(1);
+}
+
+void PV_SplitPartials_next(PV_SplitPartials* unit, int inNumSamples) {
+
+    float freq = IN0(2);
+    int partials = (int)IN0(3);
+
+    PV_GET_BUF2
+
+    float binWidthR = buf1->samples / FULLRATE;
+    //Convert bandwidth factor of freq to bins
+    int bandwidth = (int)(freq * IN0(4) * binWidthR);
+    int binStart, binEnd, currentPartial=1;
+
+    CalculateBins;
+
+	SCComplexBuf *p = (SCComplexBuf*)buf1->data;
+	SCComplexBuf *q = (SCComplexBuf*)buf2->data;
+
+    for (int i = 0; i < numbins; ++i) {
+
+        MaybeIncrementPartial(i);
+        if (i >= binStart && i < binEnd) {
+            q->bin[i] = 0.f;
+        } else {
+            q->bin[i] = p->bin[i];
+            p->bin[i] = 0.f;
+        }
     }
 }
 
-} // namespace PVPlugins
-
-PluginLoad(PVPluginsUGens) {
-    // Plugin magic
+PluginLoad(PVPlugins) {
     ft = inTable;
-    registerUnit<PVPlugins::PVPlugins>(ft, "PVPlugins");
+
+    DefinePVUnit(PV_FilterPartials)
+    DefinePVUnit(PV_SplitPartials)
 }
--- plugins/PVPlugins/PVPlugins.sc
+++ plugins/PVPlugins/PVPlugins.sc
@@ -1,10 +1,23 @@
-PVPlugins : UGen {
-	*ar { |input, gain|
-		/* TODO */
-		^this.multiNew('audio', input, gain);
-	}
-	checkInputs {
-		/* TODO */
-		^this.checkValidInputs;
+PV_FilterPartials : PV_ChainUGen {
+	*new { arg buffer, freq=440.0, partials=8, bandwidth=0.33, mode=\reject;
+
+		mode = switch(mode,
+			\reject, 0,
+			\select, 1
+		);
+
+		^this.multiNew('control', buffer, freq, partials, bandwidth, mode);
 	}
 }
+
+PV_SplitPartials : MultiOutUGen {
+	*new { arg bufferA, bufferB, freq=440.0, partials=8, bandwidth=0.33;
+		^this.multiNew('control', bufferA, bufferB, freq, partials, bandwidth);
+	}
+
+	init { arg ... theInputs;
+		inputs = theInputs;
+		^this.initOutputs(2, rate);
+	}
+}
+
